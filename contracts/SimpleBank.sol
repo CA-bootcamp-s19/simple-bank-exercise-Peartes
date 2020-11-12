d73cd1cad75418ae1966994984cfe5e21f0e2d5c
Kehinde Faleye <kenny.fale.kf@gmail.com>
2020-11-12 12:24:02 +0100

    Minor syntax error

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -117,7 +117,7 @@ contract SimpleBank {
         // Let's first subtract the amount from the user balance
         balances[msg.sender] -= withdrawAmount;
         // Now let's log the event of transfer
-        emit LogWithdrawal(msg.sender, withdrawalAmount, balances[msg.sender]);
+        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
         // Now lets send the ether to the user
         return balances[msg.sender];
     }
